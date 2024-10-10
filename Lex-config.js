4356d6abf6187cf07a1d37f7f47c83381a22058d
gurukiran2805 <66946385+gurukiran2805@users.noreply.github.com>
2024-10-10 12:43:50 +0530

    Create Lex-config.js

---
+++ Lex-config.js
@@ -0,0 +1,21 @@
+let lex_config={
+    bot_mappings:{
+    github:{
+        botId: 'SE1XOFhIQk5PMg',
+        botAliasId: 'TSTALIASID',
+        localeId: 'en_US'
+    },
+    jenkins:{
+        botAliasId: 'TSTALIASID',
+        botId: 'RjdRWk8yWFRMSg',
+        localeId: 'en_US'
+    },
+    service_now:{
+        botAliasId: 'TSTALIASID',
+        botId: 'SUw2UkJRQUxQQw',
+        localeId: 'en_US'
+    }
+},
+    region:'ap-southeast-1',
+    cognito_pool_id:'NjY1MmRmZmQtNmJmMC00ZmVjLWE0ODctY2Q2ZWI4Yzk0Yjlj'
+}
